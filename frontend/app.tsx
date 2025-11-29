686ecd19ec9e141c90f0e62b07b11cecd5d83a9f
AngeRossi1 <166455957+AngeRossi1@users.noreply.github.com>
2025-11-29 20:27:17 -0300

    levantamento front

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,1 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './src/pages/homePage';
+import Header from './src/components/layout/header';
+import Footer from './src/components/layout/footer';
+import RegisterPage from './src/pages/registroPages';
+import LoginPage from "./src/pages/loginPages";
+import CatalogoCursos from './src/pages/catalogoCursos';
+import CrearCurso from './src/pages/crearCurso';
+import CursoDetalle from './src/pages/cursosDetallesPage';
+import PerfilUsuario from './src/pages/perfilPages';
+
+const App = () => {
+  return (
+      <Router>
+      <Header />
+      <main style={{ padding: '2rem' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/registro" element={<RegisterPage />} />
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/cursos" element={<CatalogoCursos/>} />
+          <Route path="/cursos/crear" element={<CrearCurso/>}/>
+          <Route path="/cursos/:idCurso" element={<CursoDetalle/>}/>
+          <Route path="/perfil" element={<PerfilUsuario/>}/>
+          </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
+
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App';
-
+import { StrictMode } from 'react';
+import { BusquedaProvider } from './context/busquedaContexto';
+import { AuthProvider } from './context/authProviderContexto';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+  <StrictMode>
+    <AuthProvider>
+      <BusquedaProvider>
+        <App />
+      </BusquedaProvider>
+    </AuthProvider>
+  </StrictMode>
 );
