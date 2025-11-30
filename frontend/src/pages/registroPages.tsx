eed7fec3b5d5ddb56aa404b97a98b24b8bdc97f8
Alvarez Lisandro <167034264+LiFranAlvarez@users.noreply.github.com>
2025-11-29 23:40:56 -0300

    Agrego pagina de registro

---
+++ frontend/src/pages/registroPages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegisterForm from '../components/forms/registroForms';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
